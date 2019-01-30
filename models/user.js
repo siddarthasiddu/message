9eb4ad3c1e0a228422575b7b34122f633b892bfe
siddarthasiddu <siddarthasiddu96@gmail.com>
2019-01-30 15:28:39 +0530

    added front end to user profile page

--- models/user.js
+++ models/user.js
@@ -3,7 +3,12 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    status: DataTypes.STRING, 
+    firstname: DataTypes.STRING, 
+    lastname: DataTypes.STRING, 
+    gender: DataTypes.STRING,
   }, {});
   User.associate = function(models) {
     // associations can be defined here
